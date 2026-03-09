969129e6b2e00467937e10d6b2825a8786fe7ba7
Grant Garnett <grantgarnett01@outlook.com>
2026-03-09 18:31:31 -0500

    Add Piece class

---
+++ lib/piece.rb
@@ -0,0 +1,45 @@
+# This class records relevant information about a piece on a chess
+# board, such as its type, color, and information pertaining to special rules
+# (such as castling)
+class Piece
+  attr_reader :color, :type
+
+  def initialize(color, type)
+    @color = color
+    construct_piece(type)
+  end
+
+  def construct_piece(type)
+    @type = type
+
+    case type
+    when "p" then construct_pawn
+    when "R" then construct_rook
+    when "K" then construct_king
+    end
+  end
+
+  private
+
+  def construct_pawn
+    @move_by_two = true
+    @can_en_passant = false
+
+    # creates an attribute accessor that is unique to the
+    # current instance of this class
+    singleton_class.class_eval { attr_accessor "move_by_two" }
+    singleton_class.class_eval { attr_accessor "can_en_passant" }
+  end
+
+  def construct_rook
+    @can_castle = true
+
+    singleton_class.class_eval { attr_accessor "can_castle" }
+  end
+
+  def construct_king
+    @can_castle = true
+
+    singleton_class.class_eval { attr_accessor "can_castle" }
+  end
+end
---
+++ spec/piece_spec.rb
@@ -0,0 +1,58 @@
+require_relative "../lib/piece"
+
+describe Piece do
+  context("when creating a pawn") do
+    subject(:pawn_test) { described_class.new("w", "p") }
+
+    it "is a pawn" do
+      type = pawn_test.type
+      expect(type).to eq("p")
+    end
+
+    it "can move by two" do
+      move_by_two = pawn_test.move_by_two
+      expect(move_by_two).to be true
+    end
+
+    it "cannot be taken by en passant" do
+      en_passant = pawn_test.can_en_passant
+      expect(en_passant).to be false
+    end
+
+    it "returns an error when asked if it can castle" do
+      expect { pawn_test.can_castle }.to raise_error(NoMethodError)
+    end
+  end
+
+  context("when creating a rook") do
+    subject(:rook_test) { described_class.new("b", "R") }
+    it "is a rook" do
+      type = rook_test.type
+      expect(type).to eq("R")
+    end
+
+    it "can castle" do
+      expect(rook_test.can_castle).to be true
+    end
+
+    it "raises an error when asked if it can be taken by en passant" do
+      expect { rook_test.can_en_passant }.to raise_error(NoMethodError)
+    end
+  end
+
+  context("when creating a king") do
+    subject(:king_test) { described_class.new("b", "K") }
+    it "is a king" do
+      type = king_test.type
+      expect(type).to eq("K")
+    end
+
+    it "can castle" do
+      expect(king_test.can_castle).to be true
+    end
+
+    it "raises an error when asked if it can move by two" do
+      expect { king_test.move_by_two }.to raise_error(NoMethodError)
+    end
+  end
+end
