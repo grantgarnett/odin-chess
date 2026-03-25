16be1863862d59cc4f289c04d58d80ad53aa191d
Grant Garnett <grantgarnett01@outlook.com>
2026-03-25 16:02:54 -0500

    Add game initialization

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,18 @@
-require_relative "InOut"
+require_relative "in_out"
+require_relative "board"
+require_relative "player"
 
 # description to be added
 class Game
   include InOut
+
+  def initialize
+    @chess_board = Board.new
+    @player_white = Player.new("w")
+    @player_black = Player.new("b")
+    play_game
+  end
+
+  def play_game
+  end
 end
