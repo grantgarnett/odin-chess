820c11a17b401d3a846b57c43869e902e9069829
Grant Garnett <grantgarnett01@outlook.com>
2026-04-01 14:46:27 -0500

    Add tests for check_defense module

---
+++ spec/check_defense_spec.rb
@@ -0,0 +1,53 @@
+require_relative "../lib/check_defense"
+describe CheckDefense do
+  subject(:pretend_class) { Class.new { extend CheckDefense } }
+
+  context("#check_defense") do
+    before do
+      allow(pretend_class).to receive(:find_king)
+      allow(pretend_class).to receive(:find_checking_pieces)
+      allow(pretend_class).to receive(:defending_king_moves)
+    end
+
+    it "sends a message to find the king" do
+      allow(pretend_class).to receive(:find_checking_pieces).and_return(%w[dummy_val1 dummy_val2])
+      allow(pretend_class).to receive(:defending_king_moves).and_return([])
+
+      pretend_class.check_defense("w")
+      expect(pretend_class).to have_received(:find_king).with("w")
+    end
+  end
+
+  context("#find_checking_pieces") do
+    let(:king_in_check) { double("king", position: [0, 0]) }
+
+    before do
+      allow(pretend_class).to receive(:taking_moves).and_return([[0, 0]], [[0, 1]], [[0, 0]])
+    end
+
+    it "sends a message to calculate taking moves for each piece on the enemy team" do
+      pretend_class.find_checking_pieces(king_in_check, %w[piece1 piece2 piece3])
+      expect(pretend_class).to have_received(:taking_moves).exactly(3).times
+    end
+
+    it "returns an array of the pieces whose taking moves include the king's position" do
+      val = pretend_class.find_checking_pieces(king_in_check, %w[piece1 piece2 piece3])
+      expect(val).to eq(%w[piece1 piece3])
+    end
+  end
+
+  context("#find_blocking_squares") do
+    let(:king_in_check) { double("king", position: [0, 0]) }
+    let(:checking_piece) { double("piece", position: [1, 1]) }
+
+    before do
+      allow(pretend_class).to receive(:convert_to_dir).and_return(1)
+      allow(pretend_class).to receive(:non_taking_rec)
+    end
+
+    it "sends a message to find the blocking squares" do
+      pretend_class.find_blocking_squares(king_in_check, checking_piece)
+      expect(pretend_class).to have_received(:non_taking_rec)
+    end
+  end
+end
